c74e0443e8522f4fd29d82074b47ae646542f19f
Justin Boundy <jboundy@ISF.COM>
2021-03-05 14:25:19 -0500

    Removed old web front end with react spa template

---
+++ WeddingPlanner.Data/Migrations/20210213033730_Inital.cs
@@ -0,0 +1,276 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WeddingPlanner.Data.Migrations
+{
+    public partial class Inital : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "budget",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    userid = table.Column<int>(type: "int", nullable: false),
+                    taxrate = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    totalcost = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_budget", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    userid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_roles", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "schedule",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_schedule", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "expenses",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Budgetid = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_expenses", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_expenses_budget_Budgetid",
+                        column: x => x.Budgetid,
+                        principalTable: "budget",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "plan",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    budgetid = table.Column<int>(type: "int", nullable: false),
+                    Scheduleid = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_plan", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_plan_schedule_Scheduleid",
+                        column: x => x.Scheduleid,
+                        principalTable: "schedule",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "guests",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    planid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_guests", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_guests_plan_planid",
+                        column: x => x.planid,
+                        principalTable: "plan",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    rolesid = table.Column<int>(type: "int", nullable: true),
+                    Planid = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_users_plan_Planid",
+                        column: x => x.Planid,
+                        principalTable: "plan",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_users_roles_rolesid",
+                        column: x => x.rolesid,
+                        principalTable: "roles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "vendors",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    website = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    rate = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ratetype = table.Column<int>(type: "int", nullable: false),
+                    servicetype = table.Column<int>(type: "int", nullable: false),
+                    Planid = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_vendors", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_vendors_plan_Planid",
+                        column: x => x.Planid,
+                        principalTable: "plan",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "affiliate-links",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    url = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    vendorid = table.Column<int>(type: "int", nullable: false),
+                    vendorsid = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_affiliate-links", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_affiliate-links_vendors_vendorsid",
+                        column: x => x.vendorsid,
+                        principalTable: "vendors",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "images",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    vendorid = table.Column<int>(type: "int", nullable: false),
+                    imagetype = table.Column<int>(type: "int", nullable: false),
+                    location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Vendorsid = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_images", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_images_vendors_Vendorsid",
+                        column: x => x.Vendorsid,
+                        principalTable: "vendors",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_affiliate-links_vendorsid",
+                table: "affiliate-links",
+                column: "vendorsid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_expenses_Budgetid",
+                table: "expenses",
+                column: "Budgetid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_guests_planid",
+                table: "guests",
+                column: "planid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_images_Vendorsid",
+                table: "images",
+                column: "Vendorsid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_plan_Scheduleid",
+                table: "plan",
+                column: "Scheduleid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_Planid",
+                table: "users",
+                column: "Planid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_rolesid",
+                table: "users",
+                column: "rolesid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_vendors_Planid",
+                table: "vendors",
+                column: "Planid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "affiliate-links");
+
+            migrationBuilder.DropTable(
+                name: "expenses");
+
+            migrationBuilder.DropTable(
+                name: "guests");
+
+            migrationBuilder.DropTable(
+                name: "images");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "budget");
+
+            migrationBuilder.DropTable(
+                name: "vendors");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+
+            migrationBuilder.DropTable(
+                name: "plan");
+
+            migrationBuilder.DropTable(
+                name: "schedule");
+        }
+    }
+}
--- WeddingPlanner.Web/Startup.cs
+++ WeddingPlanner.Web/Startup.cs
@@ -1,12 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using WeddingPlanner.Data.Repository;
-using WeddingPlanner.Services;
-using WeddingPlanner.Services.Interfaces;
 
 namespace WeddingPlanner.Web
 {
@@ -22,17 +21,13 @@ namespace WeddingPlanner.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration["DatabaseConnection"]));
-
             services.AddControllersWithViews();
-            services.AddAuthentication()
-                .AddGoogle(googleOptions => { })
-                .AddTwitter(twitterOptions => {  })
-                .AddFacebook(facebookOptions => {  });
 
-            services.AddMvc();
-            services.AddScoped<IPlanService, PlanService>();
-
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,22 +39,32 @@ namespace WeddingPlanner.Web
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSpaStaticFiles();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
             });
         }
     }
